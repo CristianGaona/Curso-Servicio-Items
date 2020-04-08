01df6517691810a661517c63d42c7b35866a64d2
CristianGaona <crgaonas24@gmail.com>
2020-04-07 22:26:23 -0500

    Implentación de Cliente Rest de Feign, Spring Cloud

--- src/main/java/com/formaci/nbdi/springboot/app/item/controllers/ItemController.java
+++ src/main/java/com/formaci/nbdi/springboot/app/item/controllers/ItemController.java
@@ -3,10 +3,9 @@ package com.formaci.nbdi.springboot.app.item.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.formaci.nbdi.springboot.app.item.models.Item;
@@ -16,6 +15,7 @@ import com.formaci.nbdi.springboot.app.item.models.service.ItemService;
 public class ItemController {
 	
 	@Autowired
+	@Qualifier("serviceFeign")
 	private ItemService itemService;
 	
 	@GetMapping("/listar")
